4baffad95b9f0f0d777ecfdbbe6d605123652299
Sandeep Kumar <149479214+Sandeepkumar@users.noreply.github.com>
2024-04-11 12:14:26 +0530

    first commit

---
+++ Shery 1/script.js
@@ -0,0 +1,19 @@
+Shery.mouseFollower({
+    // skew: true,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+});
+Shery.makeMagnet(" h1", {
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+});
+
+Shery.textAnimate("h1",{
+    //Parameters are optional.
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
---
+++ Shery 3/script.js
@@ -0,0 +1,25 @@
+Shery.mouseFollower();
+Shery.makeMagnet("#nav-div h4", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  //   duration: 1,
+  });
+  Shery.makeMagnet("h2", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  //   duration: 1,
+  });
+  Shery.textAnimate("h1",{
+    //Parameters are optional.
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
+Shery.imageEffect("img",{
+    style:3,
+    // debug:true,
+    // gooey: true,
+    
+    
+});
